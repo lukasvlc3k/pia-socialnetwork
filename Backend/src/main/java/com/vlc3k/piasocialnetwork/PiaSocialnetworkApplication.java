dc6a2a854fe041a43d857ad50034854a03c28fc9
Lukáš Vlček <vlcek@net-inout.cz>
2022-01-07 13:46:16 +0100

    Backend (java Spring) init

---
+++ Backend/src/main/java/com/vlc3k/piasocialnetwork/PiaSocialnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.vlc3k.piasocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiaSocialnetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiaSocialnetworkApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/vlc3k/piasocialnetwork/PiaSocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlc3k.piasocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiaSocialnetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
